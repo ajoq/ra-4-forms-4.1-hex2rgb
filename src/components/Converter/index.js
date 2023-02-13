59a1f411cf798e47c39c50bc337e271828f7b36f
Konstantin Beloglazov <ajoq@ya.ru>
2023-02-13 11:40:55 +0300

    done

---
+++ src/components/Converter/index.js
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import hexRgb from '../hexRgb';
+import './index.css';
+
+function Converter() {
+    const [conv, setConv] = useState({
+        hex: '',
+        rgb: 'rgb(red, green, blue)',
+        bodyColor: '',
+    });
+
+    const handleInput = (evt) => {
+        const hexValue = evt.target.value;
+
+        setConv((prev) => ({
+            ...prev,
+            hex: hexValue,
+            rgb: 'rgb(red, green, blue)',
+            bodyColor: '',
+        }));
+
+        if (hexValue.length < 6) return;
+        let color = hexValue;
+        let hexRes = hexRgb(hexValue);
+        if (!hexRes) {
+            color = 'e74c3c';
+            hexRes = 'Ошибка!';
+        }
+
+        setConv((prev) => ({
+            ...prev,
+            rgb: hexRes,
+            bodyColor: '#' + color,
+        }));
+    };
+
+    return (
+        <>
+            <div
+                className="converter"
+                style={{ backgroundColor: conv.bodyColor }}
+            >
+                <div className="fields">
+                    <div className="before-input">
+                        <input
+                            name="hex"
+                            className="input"
+                            type="text"
+                            value={conv.hex}
+                            onChange={handleInput}
+                            placeholder="34495e"
+                        />
+                    </div>
+                    <p className="result">{conv.rgb}</p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Converter;
